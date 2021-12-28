037f7e3fefefe3e500b418b5de3f91a23e0149e7
ankitsingh060 <ankitsingh10026@gmail.com>
2021-12-29 01:51:27 +0530

    projcet commited

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("We are connected bro/sis...");
+});
+
+// const kittySchema = new mongoose.Schema({
+//     name: String
+//   });
+
+// const Kitten = mongoose.model('Kitten', kittySchema);
+
